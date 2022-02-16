17aa018b801c21358fda6b2d63edf8e11b9be314
Gábor Koleszár <gabor.koleszar@protonail.ch>
2022-02-16 20:41:52 +0100

    Room database classes

---
+++ app/src/main/java/com/example/dictionaryapp/data/local/entity/WordInfoEntity.kt
@@ -0,0 +1,24 @@
+package com.example.dictionaryapp.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.dictionaryapp.domain.model.Meaning
+import com.example.dictionaryapp.domain.model.WordInfo
+
+@Entity
+data class WordInfoEntity(
+    val word: String,
+    val phonetic: String,
+    val origin: String,
+    val meanings: List<Meaning>,
+    @PrimaryKey val id: Int? = null
+) {
+    fun toWordInfo(): WordInfo {
+        return WordInfo(
+            word = word,
+            phonetic = phonetic,
+            origin = origin,
+            meanings = meanings
+        )
+    }
+}
